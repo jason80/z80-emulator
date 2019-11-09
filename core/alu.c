a9891c3caa734223eb439d7425cc0926ce982831
Javier Candales (Jasón) <javier_candales@yahoo.com.ar>
2019-11-09 12:29:18 -0300

    Added all SUB A, ... instructions

--- core/alu.c
+++ core/alu.c
@@ -4,6 +4,7 @@
 
 void add(uint8_t* reg8);
 void adc(uint8_t* reg8);
+void sub(uint8_t* reg8);
 
 void alu(uint8_t operation, uint8_t* reg8) {
 	switch(operation) {
@@ -14,6 +15,7 @@ void alu(uint8_t operation, uint8_t* reg8) {
 		adc(reg8); cpu->ts = 4;
 		break;
 	case 2:		// SUB
+		sub(reg8); cpu->ts = 4;
 		break;
 	case 3:		// SBC
 		break;
@@ -40,6 +42,7 @@ void alu_n(uint8_t operation) {
 		adc(&BRL); cpu->ts = 7;
 		break;
 	case 2:		// SUB
+		sub(&BRL); cpu->ts = 7;
 		break;
 	case 3:		// SBC
 		break;
@@ -65,6 +68,7 @@ void alu_indirect(uint8_t operation, uint16_t* reg16) {
 		adc(&cpu->mem[*reg16 + BRL]); cpu->ts = 19;
 		break;
 	case 2:		// SUB
+		sub(&cpu->mem[*reg16 + BRL]); cpu->ts = 19;
 		break;
 	case 3:		// SBC
 		break;
@@ -109,6 +113,25 @@ void adc(uint8_t* reg8) {
 	// Overflow pv
 	FLAG_PV = 0;
 	if ((A & 0x80) == (*reg8 & 0x80))
+		if ((A & 0x80) != (result & 0x80))
+			FLAG_PV = 1;
+	
+	FLAG_C = result & 0x100 ? 1 : 0;
+	
+	A = (uint8_t) result;
+}
+
+void sub(uint8_t* reg8) {
+	uint16_t result = A - *reg8;
+	
+	FLAG_S = result < 0 ? 1 : 0;
+	FLAG_Z = result == 0 ? 1 : 0;
+	
+	FLAG_H = (((A & 0x0F) - (*reg8 & 0x0F)) & 0x10) > 0 ? 1 : 0;
+	
+	// Overflow pv
+	FLAG_PV = 0;
+	if ((A & 0x80) != (*reg8 & 0x80))
 		if ((A & 0x80) != (result & 0x80))
 			FLAG_PV = 1;
 	
