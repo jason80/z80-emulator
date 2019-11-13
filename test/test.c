72dfedfed5d52e4ea70023a90d03a9d03273ed5f
Javier Candales (Jasón) <javier_candales@yahoo.com.ar>
2019-11-13 19:01:00 -0300

    Created CP r test

--- test/test.c
+++ test/test.c
@@ -237,6 +237,12 @@ int main(void) {
 		return CU_get_error();
 	}
 	
+	if (	(NULL == CU_add_test(cp_suite, "CP r", cp_A_r_test))
+		) {
+		CU_cleanup_registry();
+		return CU_get_error();	
+	}
+	
 	//cpu_suite->fActive = CU_FALSE;
 	//load_8bit_suite->fActive = CU_FALSE;
 	//load_16bit_suite->fActive = CU_FALSE;
