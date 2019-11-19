197d239e07a0a7277b2193e015e473c7b740568b
Javier Candales (Jasón) <javier_candales@yahoo.com.ar>
2019-11-18 22:50:12 -0300

    Created ADC HL, rr test

--- test/arithm_16bit_suite.c
+++ test/arithm_16bit_suite.c
@@ -28,3 +28,20 @@ void add16_hl_rr_test(void) {
 	
 	CU_ASSERT(HL == 0x5353);
 }
+
+void adc16_hl_rr_test(void) {
+	cpu_reset();
+	
+	HL = 0x5437;
+	BC = 0x2222;
+	FLAG_C = 1;
+	
+	cpu->mem[0] = 0xED;
+	cpu->mem[1] = 0x4A;	// ADC HL, BC
+	
+	cpu_fetch();
+	cpu_execute();
+	
+	CU_ASSERT(HL == 0x765A);
+	CU_ASSERT(FLAG_C == 0);
+}
--- test/arithm_16bit_suite.h
+++ test/arithm_16bit_suite.h
@@ -5,5 +5,6 @@ int init_arithm_16bit_suite(void);
 int clean_arithm_16bit_suite(void);
 
 void add16_hl_rr_test(void);
+void adc16_hl_rr_test(void);
 
 #endif
