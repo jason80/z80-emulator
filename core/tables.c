3352f6f0b6068b1bc3fcc5fc91bc90df652304c1
Javier Candales (Jasón) <javier_candales@yahoo.com.ar>
2019-12-09 20:58:26 -0300

    Fixed cc table in tables.c

--- core/tables.c
+++ core/tables.c
@@ -56,10 +56,10 @@ uint16_t* table_rp2(int v) {
 
 int table_cc(int v) {
 	switch(v) {
-		case 0: return FLAG_Z == 1 ? 1 : 0;
-		case 1: return FLAG_Z == 0 ? 1 : 0;
-		case 2: return FLAG_C == 1 ? 1 : 0;
-		case 3: return FLAG_C == 0 ? 1 : 0;
+		case 0: return FLAG_Z == 0 ? 1 : 0;
+		case 1: return FLAG_Z == 1 ? 1 : 0;
+		case 2: return FLAG_C == 0 ? 1 : 0;
+		case 3: return FLAG_C == 1 ? 1 : 0;
 		case 4: return FLAG_PV == 0 ? 1 : 0;
 		case 5: return FLAG_PV == 1 ? 1 : 0;
 		case 6: return FLAG_S == 0 ? 1 : 0;
