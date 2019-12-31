40a8a401a1b80e0f4198168a0e7f2d6e80e60d1e
Javier Candales (Jasón) <javier_candales@yahoo.com.ar>
2019-12-30 23:12:50 -0300

    Disassembled instructions: NOP EX AF, AF' DJNZ, e

--- disassembler/disassembler.c
+++ disassembler/disassembler.c
@@ -1,5 +1,8 @@
 #include "disassembler.h"
 
+#include <string.h>
+#include <stdio.h>
+
 typedef union {
 	uint8_t byte;
 	struct {
@@ -28,6 +31,14 @@ typedef union {
 	};
 } opcode_t;
 
+// For relative jumps, gets the absolute from a relative value
+uint16_t get_absolute_address(uint8_t relative, uint16_t address);
+
+uint16_t dis_x0(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code);
+uint16_t dis_x1(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code);
+uint16_t dis_x2(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code);
+uint16_t dis_x3(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code);
+
 uint16_t disassemble(uint8_t mem[], uint16_t address, char* code) {
 	opcode_t opcode;
 	uint8_t prefix;
@@ -37,7 +48,7 @@ uint16_t disassemble(uint8_t mem[], uint16_t address, char* code) {
 	
 	// Detect prefix
 	if (0xDD == byte || 0xFD == byte) {
-		cpu->prefix = byte;
+		prefix = byte;
 		byte = mem[address]; address ++;
 	}
 	
@@ -48,6 +59,80 @@ uint16_t disassemble(uint8_t mem[], uint16_t address, char* code) {
 	} else if (0xCB == byte) {
 		
 	} else {
-		
+		switch (opcode.x) {
+		case 0:	// x = 0
+			return dis_x0(opcode, mem, &address, code);
+		case 1: // x = 1
+			return dis_x1(opcode, mem, &address, code);
+		case 2: // x = 2
+			return dis_x2(opcode, mem, &address, code);
+		case 3: // x = 3
+			return dis_x3(opcode, mem, &address, code);
+		}
 	}
+}
+
+uint16_t dis_x0(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code)  {
+	
+	uint8_t byte;
+	
+	switch (opcode.z) {
+	case 0: // z = 0
+		switch (opcode.y) {
+		case 0: // y = 0
+			strcpy(code, "NOP");
+			break;
+		case 1: // y = 1
+			strcpy(code, "EX AF, AF'");
+			break;
+		case 2: // y = 2
+			byte = mem[*address]; (*address) ++;
+			sprintf(code, "DJNZ %.4Xh", get_absolute_address(byte, *address));
+			//djnz();				// DJNZ e
+			break;
+		case 3: // y = 3
+			//jump_relative();	// JR e
+			break;
+		case 4: // y = 4
+			//jr_nz_rel();		// JR NZ, e
+			break;
+		case 5: // y = 5
+			//jr_z_rel();			// JR Z, e
+			break;
+		case 6: // y = 6
+			//jr_nc_rel();		// JR NC, e
+			break;
+		case 7: // y = 7
+			//jr_c_rel();			// JR C, e
+			break;
+		}
+		break;
+	}
+	
+	return *address;
+}
+
+uint16_t dis_x1(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code) {
+	return *address;
+}
+
+uint16_t dis_x2(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code) {
+	return *address;
+}
+
+uint16_t dis_x3(opcode_t opcode, uint8_t mem[], uint16_t* address, char* code) {
+	return *address;
+}
+
+uint16_t get_absolute_address(uint8_t relative, uint16_t address) {
+	relative += 2;
+	
+	union {
+		uint8_t uns;
+		int8_t sig;
+	} value;
+	
+	value.uns = relative;
+	
+	return address + value.sig;
 }
--- disassembler/test.c
+++ disassembler/test.c
@@ -4,6 +4,22 @@
 
 int main() {
 	
-	printf("Hola\n");
+	uint8_t mem[20];
+	char code[30];
+	
+	mem[0] = 0x00;		// NOP			// 00 000 000
+	mem[1] = 0x08;		// EX AF, AF'	// 00 001 000
+	mem[2] = 0x10;		// DJNZ 05h		// 00 010 000
+	mem[3] = 0x03;		// 05h
+	
+	disassemble(mem, 0, code);
+	printf("%s\n", code);
+	
+	disassemble(mem, 1, code);
+	printf("%s\n", code);
+	
+	disassemble(mem, 2, code);
+	printf("%s\n", code);
+	
 	return 0;
 }
